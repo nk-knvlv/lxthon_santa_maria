8c356b1f53f75102887a6cb3b1780d28ce6e63c6
nk-knvlv <nk_knvlv@mail.ru>
2025-06-13 16:57:00 +0300

    update; scrum_builder_extension; добавил папку хром расширения для проекта

--- scrum_builder.py
+++ scrum_builder.py
@@ -1,8 +1,13 @@
 from datetime import timedelta
 from .model import Sprint
 
-
+#TODO сделать класс синглтоном
 class ScrumBuilder:
     sprint_duration: timedelta
 
-    def create_sprint(self, ): ->
+
+    def __init__(self):
+
+
+    def create_sprint(self, )-> Sprint:
+        return Sprint
