65b057ef6b11d9a44407ed4a3d21732b8be2bfb1
Binobinos <binobinos.dev@gmail.com>
2025-06-13 13:28:18 +0300

    Create -the function sum

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
 
-# Press the green button in the gutter to run the script.
+def sum_int(a: int, b: int):
+    return a + b
+
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(f"Сумма чисел 2 + 2 = {sum_int(2, 2)}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
