6fbc67cae52ac56790b7bf4994d94ed4560df3a9
nk-knvlv <nk_knvlv@mail.ru>
2025-06-14 13:02:30 +0300

    update;link

--- scrum_builder_extension/popup.js
+++ scrum_builder_extension/popup.js
@@ -7,45 +7,10 @@ class ScrumBuilder {
         const masterInviteBtn = document.getElementById("start");
         masterInviteBtn.addEventListener("click", async () => {
             console.log('test1')
-            const meetIdInput = document.getElementById("meet-id");
-            const meetId = meetIdInput.value
+            const meetLinkInput = document.getElementById("meet-link");
+            const meetId = meetLinkInput.value.split('/')[3]
             await this.inviteMaster(meetId)
 
-        });
-    }
-
-    static test() {
-        chrome.tabs.query({active: true}, async function (tabs) {
-            let tab = tabs[0];
-            if (tab) {
-
-                let conn = document.getElementById("conn");
-                conn.innerHTML = 'false';
-
-                try {
-                    const response = await fetch("http://localhost:8000/test", {
-                        method: "GET",
-                        headers: {"Accept": "text/plain"}
-                    });
-
-                    if (response.ok) {
-                        const text = await response.text(); // получаем текст ответа
-                        console.log(text); // выводим в консоль
-                        conn.innerHTML = 'success';
-
-                        let result = document.getElementById("result");
-                        result.innerHTML = text;
-                    } else {
-                        console.error("HTTP error", response.status);
-                        conn.innerHTML = 'error';
-                    }
-                } catch (error) {
-                    console.error("Fetch error:", error);
-                    conn.innerHTML = 'fetch error';
-                }
-            } else {
-                alert("There are no active tabs");
-            }
         });
     }
 
